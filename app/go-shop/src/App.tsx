133daeed6dc0d1b29475a56e542330e9ebc78fe5
blvu <013ayu@gmail.com>
2023-07-06 02:44:17 +0800

    Update routes

--- app/go-shop/src/App.tsx
+++ app/go-shop/src/App.tsx
@@ -4,41 +4,38 @@ import {Route, Routes} from 'react-router-dom'
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Categories from './pages/Categories';
+import Edit from './pages/Edit';
+import Details from './pages/Details';
 import Login from './pages/Login';
 import LogOut from './pages/LogOut';
 import Register from './pages/Register';
+import Profile from './pages/Profile';
 import Error404 from './pages/Error404';
 import Messages from './pages/Messages';
 import EditProfile from './pages/EditProfile';
 
 import './App.css';
 
-//import axios from 'axios';
-
 function App() {
     /** TODO:
-     * <Route path="/categories/:category/:id/details" component={Details} />
-     * <Route path="/categories/:category/:id/edit" component={Edit} />
-     * <Route path="/auth/logout" exact render={LogOut} />
      * <Route path='/add-product' exact component={CreateSell} />;
-     * <Route path='/profile/:id' exact component={Profile} />;
-     * <Route path='/profile/:id/edit' exact component={EditProfile} />;
-     * <Route path='/messages' exact component={Messages} />;
-     * <Route path='/messages/:id' exact component={Messages} />;
-     * <Route component={Error404} />
      */
     return (
         <>
             <Header/> 
             <Routes>
-                <Route path="/" element={<Categories/>}/>
-                <Route path="/categories/:category" element={<Categories/>}/>
-                <Route path="/auth/login" element={<Login/>}/>
-                <Route path="/auth/logout" element={<LogOut/>}/>
-                <Route path="/auth/register" element={<Register/>}/>
-                <Route path="/messages/:id" element={<Messages/>}/>
-                <Route path='/profile/:id/edit' element={<EditProfile/>} />;
-                <Route path="*" element={<Error404/>}/>
+               <Route path="/" element={<Categories/>}/>
+               <Route path="/categories/:category" element={<Categories/>}/>
+               <Route path="/categories/:category/:id/details" element={<Details/>} />
+               <Route path="/categories/:category/:id/edit" element={<Edit/>} />
+               <Route path="/auth/login" element={<Login/>}/>
+               <Route path="/auth/logout" element={<LogOut/>}/>
+               <Route path="/auth/register" element={<Register/>}/>
+               <Route path='/profile/:id' element={<Profile/>} />;
+               <Route path='/profile/:id/edit' element={<EditProfile/>} />;
+               <Route path="/messages/:id" element={<Messages/>}/>
+               <Route path='/messages/:id' element={<Messages/>} />;
+               <Route path="*" element={<Error404/>}/>
             </Routes>
             <Footer/>
         </>
